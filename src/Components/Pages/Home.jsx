3fc09797307c71a7cb7017c839c61bb31d812bf8
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-07-12 16:39:15 +0530

    Home page added

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,13 @@
-
+import Navbar from "../Shared/Navbar"
+import Landing from "../ui/Landing"
+import Footer from "../Shared/Footer"
 const Home = () => {
   return (
-    <div>
-        <h1>navbar</h1>
-      
-    </div>
+    <>
+    <Navbar />
+    <Landing />
+    <Footer />
+    </>
   )
 }
 
