e02bafe0c60aff77ced70b03e9ed077bce84d049
anuj846k <anuj846k@gmail.com>
2024-08-12 07:21:40 +0530

    ImageCarousel fixed

--- src/Components/ui/ImageCarousel.jsx
+++ src/Components/ui/ImageCarousel.jsx
@@ -4,7 +4,8 @@ import event2 from "../../assets/img/event2.jpg";
 import event3 from "../../assets/img/event3.jpg";
 import event4 from "../../assets/img/event4.jpg";
 import event6 from "../../assets/img/event6.jpg";
-import { MdEventRepeat } from "react-icons/md";
+import { MdEventRepeat, MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Carousel = () => {
   const [images] = useState([event1, event2, event3, event4, event6]);
@@ -26,48 +27,39 @@ const Carousel = () => {
   }, [nextSlide]);
 
   return (
-    <div className="relative mb-20 ">
-      <h1 className="md:text-6xl text-3xl pl-10 pt-8 font-bold mb-5 mr-2 md:mr-0 flex items-center">
-        Explore our fun events <MdEventRepeat size={80} className="mr-10 text-yellow-800" />
+    <div className="relative mb-20 p-2 ">
+      <h1 className="md:text-6xl text-3xl  pt-8 font-bold mb-5 mr-2 md:mr-0 flex items-center">
+        Explore our fun events{" "}
+        <MdEventRepeat size={80} className="mr-10 text-yellow-800" />
       </h1>
+
       <div className="overflow-hidden rounded-3xl  items-center flex justify-center bg-green-700">
         <img
-          className="md:w-screen md:h-[60vh] h-[40vh] w-[40vh] p-2 object-cover rounded-2xl"
+          className="md:w-screen md:h-[60vh] h-[40vh] w-[50vh] p-2 object-cover rounded-3xl"
           src={images[currentImageIndex]}
           alt={`Slide ${currentImageIndex + 1}`}
         />
-      </div>
-      <div
-        className="absolute inset-y-0 left-0 flex items-center justify-center w-12 cursor-pointer"
-        onClick={prevSlide}
-      >
-        <svg
-          className="h-6 w-6 text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
+        <div
+          onClick={prevSlide}
+          className="absolute md:left-5 hidden md:block  top-1/2 rounded-full hover:bg-black bg-black bg-opacity-40 transform -translate-y-1/2 cursor-pointer p-3 "
         >
-          <path
-            fillRule="evenodd"
-            d="M4.293 9.293l7-7a1 1 0 0 1 1.414 1.414L7.414 10l5.293 5.293a1 1 0 0 1-1.414 1.414l-7-7a1 1 0 0 1 0-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-      <div
-        className="absolute inset-y-0 right-0 flex items-center justify-center w-12 cursor-pointer"
-        onClick={nextSlide}
-      >
-        <svg
-          className="h-6 w-6 text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
+          <MdOutlineArrowBackIosNew className="text-white" size={30} />
+        </div>
+        <div
+          onClick={nextSlide}
+          className="absolute right-5 hidden md:block top-1/2 rounded-full hover:bg-black bg-black bg-opacity-40 transform -translate-y-1/2 cursor-pointer p-3 "
         >
-          <path
-            fillRule="evenodd"
-            d="M15.707 9.293l-7-7a1 1 0 0 0-1.414 1.414L12.586 10l-5.293 5.293a1 1 0 0 0 1.414 1.414l7-7a1 1 0 0 0 0-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+          <MdOutlineArrowForwardIos className="text-white" size={30} />
+        </div>
+      </div>
+
+      <div className="flex justify-center mt-7 ">
+        <Link
+          to="/event"
+          className="text-2xl md:text-4xl py-3 px-10 bg-yellow-800 text-white rounded-full hover:bg-green-800"
+        >
+          Events
+        </Link>
       </div>
     </div>
   );
--- src/Components/ui/ReviewCarousel.jsx
+++ src/Components/ui/ReviewCarousel.jsx
@@ -1,80 +1,26 @@
-import { useState } from "react";
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-
-const feedbackData = [
-  {
-    id: 1,
-    name: 'Sarah Thompson',
-    feedback: 'Sip & Play Café is my go-to spot for relaxing with friends over a delicious boba coffee. The ambiance is perfect for unwinding, and their board game collection is impressive!',
-  },
-  {
-    id: 2,
-    name: 'Alex Martinez',
-    feedback: 'As a board game enthusiast, Sip & Play Café exceeded my expectations. The staff is friendly, the boba coffee is delightful, and the atmosphere is ideal for both casual and competitive gaming sessions.',
-  },
-  {
-    id: 3,
-    name: 'Emily Nguyen',
-    feedback: 'I love the cozy vibes at Sip & Play Café. Whether I want to enjoy a refreshing boba coffee or challenge friends to board games, this place never disappoints. Highly recommended!',
-  },
-];
+import { MdStars } from "react-icons/md";
 
 const ReviewCarousel = () => {
-  const { ref, inView } = useInView({
-    threshold: 0.5, // Trigger animation when 50% of the component is visible
-  });
-
-  const animationVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  const [currentFeedback, setCurrentFeedback] = useState(0);
-
-  const nextFeedback = () => {
-    setCurrentFeedback((currentFeedback + 1) % feedbackData.length);
-  };
-
-  const prevFeedback = () => {
-    setCurrentFeedback((currentFeedback - 1 + feedbackData.length) % feedbackData.length);
-  };
-
   return (
-    <div ref={ref}>
-      <motion.div
-        initial="hidden"
-        animate={inView ? "visible" : "hidden"}
-        variants={animationVariants}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="justify-center py-10 px-10">
-          <div className="relative bg-white rounded-2xl shadow-lg overflow-hidden">
-            <div className="absolute inset-0">
-              <div className="absolute inset-0 bg-amber-100 "></div>
-            </div>
-            <div className="relative px-6 py-8">
-              <div className="text-center">
-                <p className="md:text-5xl pb-2 font-mono leading-6 font-semibold">Customer Feedback</p>
-                <p className="mt-2 md:text-xl font-mono md:mb-5">{feedbackData[currentFeedback].feedback}</p>
-                <p className="mt-2 text-sm">- {feedbackData[currentFeedback].name}</p>
-              </div>
-            </div>
-            <div className="absolute bottom-0 inset-x-0 pb-8 flex md:justify-around justify-between">
-              <button onClick={prevFeedback} className="text-black hover:text-gray-700 focus:outline-none">
-                <svg className="h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
-                </svg>
-              </button>
-              <button onClick={nextFeedback} className="ml-4 text-black hover:text-gray-700 focus:outline-none">
-                <svg className="h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
-                </svg>
-              </button>
-            </div>
-          </div>
+    <div className="mb-20">
+      <div className="items-center flex justify-center mb-10  ">
+        <MdStars size={70} className="text-[#004D43]"/>
+        <h1 className="text-6xl font-bold text-[#004D43] ">Customer Feedback</h1>
+        <MdStars size={70} className="text-[#004D43]" />
+      </div>
+      <div className="bg-red-200 h-[50vh] p-20 flex gap-10">
+        <div className="h-[35vh]  w-[35vh] rounded-xl bg-yellow-200">
+
         </div>
-      </motion.div>
+        <div className="h-[35vh]  w-[35vh] rounded-xl bg-yellow-200">
+
+        </div><div className="h-[35vh]  w-[35vh] rounded-xl bg-yellow-200">
+
+</div><div className="h-[35vh]  w-[35vh] rounded-xl bg-yellow-200">
+
+</div>
+      </div>
+
     </div>
   );
 };
